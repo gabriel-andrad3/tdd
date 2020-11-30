ed7151d7b0cb2342f381da59a89afe871f7abee0
Gabriel Andrade <gandradevini@gmail.com>
2020-11-30 11:29:57 -0300

    feature delta, more test cases

--- test/bhaskaraTest.js
+++ test/bhaskaraTest.js
@@ -3,8 +3,16 @@ const bhaskara = require('../src/bhaskara');
 
 describe('Bhaskara', () => {
     context('delta', () => {
-        it('should calculate delta', () => {            
+        it('results positive', () => {
             bhaskara.calculateDelta(4, 2, -6).should.equal(100);
         })
+        
+        it('results zero', () => {
+            bhaskara.calculateDelta(1, -10, 25).should.equal(0);
+        })
+
+        it('results negative', () => {
+            bhaskara.calculateDelta(7, 3, 4).should.equal(-103);
+        }) 
     })
 })
